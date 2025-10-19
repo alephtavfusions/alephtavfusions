a2aa0d41ee706b926adb703eafce4e3e035fc879
raphaelbdias <raphaelbdias@users.noreply.github.com>
2025-10-19 02:30:48 +0000

    deploy: 4ab6f9d50ad9ed5370f12fb8714d2ae9991653d6

---
+++ _next/static/chunks/app/services/web-design/page-7e4580ff008c44a9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[563],{2252:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,3082,23))},3082:()=>{}},e=>{e.O(0,[649,619,441,255,358],()=>e(e.s=2252)),_N_E=e.O()}]);
